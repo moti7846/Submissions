a3a2d9d42908aa7f414743c605118adeb3c71fa7
moti7846 <8563816@gmail.com>
2025-07-01 11:13:53 +0300

    Revert "."
    This reverts commit 7d0411bb4569f90714be547539e588c5f321af90.

---
+++ task.js
@@ -0,0 +1,72 @@
+// let arr = [];
+// arr.push("moti")
+// arr.push(true)
+// arr.push(805)
+// console.log(arr.pop(0));
+// console.log(arr);
+// const str = 'BeptQdWZim'.split('');
+// let index = []
+// for(let i = 0;i<str.length;i++)
+// {
+//     if(str[i] >= 'A' && str[i] <= 'Z')
+//     {
+//         index.push(i)
+//     }
+// }
+// for(let i = 0;i<index.length;i++)
+// {
+//     str.splice(index[i]- i , 1);
+// }
+// console.log(str.join(''));
+// let me = {
+//     name:"moti",
+//     age:22,
+//     hobbies: [1,2,3]
+// }
+// for(const key in me)
+// {
+//     console.log(key + " " + me[key]);
+    
+// }
+// let name = process.argv[2 = "a"];
+// let age = process.argv[3] - 2025;
+// console.log(name ,age);
+// function Name(nam1 = '')
+// {
+//     console.log(nam1.toUpperCase());
+// }
+// Name("moti");
+
+// function Age(yuor)
+// {
+//     return 2025 - yuor
+// }
+// a = Age(22)
+// console.log(a);
+// const add = (num1, num2) => {
+//     return num1+num2; 
+// }
+// const subtract = (num1, num2) => {
+//     return divide(num1-num2,10); 
+// }
+// const multiply = (num1, num2) => {
+//     return subtract(num1*num2,5); 
+// }
+// const divide = (num1, num2) => {
+//     return num1/num2; 
+// }
+// function start( callback){
+//     console.log(callback(5,2));
+//     ;
+// }
+// start(multiply);
+let cars = [
+  {Ferrari: 340},
+  {Lamborghini: 325},
+  {Tesla: 250},
+  {BMW: 305},
+  {Toyota: 180}
+];
+let a = cars.map(
+    
+)
