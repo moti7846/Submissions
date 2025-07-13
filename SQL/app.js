fd28140a5eb392ba3782979b72b376015353715b
moti7846 <8563816@gmail.com>
2025-07-13 11:47:03 +0300

    added try and catch

--- SQL/app.js
+++ SQL/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import {config } from 'dotenv';
-import { question1, question2, question3 } from './question_db.js';
+import { getCustomersFromFranceCount, getEmployeesReportingToManager1143, getClassicCarsProductCount } from './question_controlers.js';
 config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use("/question1" , question1)
-app.use("/question2" , question2)
-app.use("/question3" , question3)
+app.get("/question1" , getCustomersFromFranceCount)
+app.get("/question2" , getEmployeesReportingToManager1143)
+app.get("/question3" , getClassicCarsProductCount)
 
 app.use((req, res)=>{
     res.status(404).json({msg: "Route not found."});
